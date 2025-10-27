aa4b720ad0f5217f47badda5a2344a64856427cf
Pratik-Kalaghan <pratikkalghan8@gmail.com>
2025-10-27 12:00:58 +0530

    First Update in normal print statement. Done on 27-10-2025

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         // Print "Hello, World!" to the console
         System.out.println("Hello, World!");
+        System.out.println("This is the updated code, Learning to push the updated code");
     }
 }
 
