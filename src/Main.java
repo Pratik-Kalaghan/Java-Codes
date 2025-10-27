0c47dc1783fc7152af65c34f2c612491cf1a80b8
Pratik-Kalaghan <pratikkalghan8@gmail.com>
2025-10-27 12:04:18 +0530

    Second Update(Worked on code Restructuring) Done on 27-10-2025

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         // Print "Hello, World!" to the console
         System.out.println("Hello, World!");
-        System.out.println("This is the updated code, Learning to push the updated code");
+        System.out.println("This is the updated code"); //Learning to Push the updated code
     }
 }
 
